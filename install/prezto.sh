687534083b88e2063f5472e3d876cc46db194882
Jim Sangwine <jim@competa.com>
2016-02-28 00:21:43 +0100

    Fix symlinks for zprezto

--- install/prezto.sh
+++ install/prezto.sh
@@ -7,7 +7,7 @@ source "$DOTFILES_DIR"/runcom/zprezto/.zprofile
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 setopt EXTENDED_GLOB
-for rcfile in "$DOTFILES_DIR"/runcom/zprezto/^README.md(.N); do
+for rcfile in "$DOTFILES_DIR"/runcom/zprezto/.* ; do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
