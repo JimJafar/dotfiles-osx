6aae9597286bd81b0d888f133afb38253b4fc910
Jim Sangwine <jim@competa.com>
2016-02-27 22:02:39 +0100

    Added prezto

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 . "$DOTFILES_DIR/install/bash.sh"
 . "$DOTFILES_DIR/install/npm.sh"
 . "$DOTFILES_DIR/install/pip.sh"
+. "$DOTFILES_DIR/install/prezto.sh"
 
 if [ "$(uname)" == "Darwin" ]; then
     . "$DOTFILES_DIR/install/brew-cask.sh"
