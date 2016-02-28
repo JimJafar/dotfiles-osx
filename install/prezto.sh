47272c6d8785fe40095f4d2ec57dfbc72f737ee5
Jim Sangwine <jim@sangwine.net>
2016-02-28 20:44:42 +0100

    Simplify presto dot file linking

--- install/prezto.sh
+++ install/prezto.sh
@@ -2,12 +2,17 @@ echo "Setting up zsh and zprezto..."
 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-setopt EXTENDED_GLOB
-for rcfile in "$DOTFILES_DIR"/runcom/zprezto/!(.|..) ; do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/$(basename $rcfile)"
-done
+# link to prezto dotfiles
+
+ln -sfv "$DOTFILES_DIR/runcom/zprezto/.zlogin" ~
+ln -sfv "$DOTFILES_DIR/runcom/zprezto/.zlogout" ~
+ln -sfv "$DOTFILES_DIR/runcom/zprezto/.zpreztorc" ~
+ln -sfv "$DOTFILES_DIR/runcom/zprezto/.zprofile" ~
+ln -sfv "$DOTFILES_DIR/runcom/zprezto/.zshenv" ~
+ln -sfv "$DOTFILES_DIR/runcom/zprezto/.zshrc" ~
 
 # Set zsh as default shell
+
 chsh -s /bin/zsh
 
 echo "All done! Please run 'source ~/.zprofile' or just fire up a new terminal!"
