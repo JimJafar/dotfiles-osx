1acaf528456591c00ef4d80e9004e6d3d2127084
Jim Sangwine <jim@sangwine.net>
2016-04-10 02:01:12 +0200

    Fixed a bug in the launchd installer

--- install/osx.sh
+++ install/osx.sh
@@ -4,12 +4,16 @@ ln -sfv "$DOTFILES_DIR/osx/launchd-scripts" $HOME
 for directory in LaunchAgents LaunchDaemons; do
   if [ -d "$HOME/Library/$directory" ]; then
     echo "found existing $directory"
-    for filename in "$DOTFILES_DIR/osx/$directory"/*.plist; do
-      echo "symlinking $filename"
-      ln -sfv "$DOTFILES_DIR/osx/$directory/$filename" "$HOME/Library/$directory/"
+    for file in "$DOTFILES_DIR/osx/$directory"/*.plist; do
+      echo "symlinking $file"
+      ln -sfv "$DOTFILES_DIR/osx/$directory/${file##*/}" "$HOME/Library/$directory/"
+      launchctl load "$DOTFILES_DIR/osx/$directory/${file##*/}"
     done
   else
     echo "symlinking $directory"
     ln -sfv "$DOTFILES_DIR/osx/$directory" "$HOME/Library/"
+    for file in "$DOTFILES_DIR/osx/$directory"/*.plist; do
+      launchctl load "$DOTFILES_DIR/osx/$directory/${file##*/}"
+    done
   fi
 done
