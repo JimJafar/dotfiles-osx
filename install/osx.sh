0db8837c8de98ae21fef5cb2a1abddd86d6fd2d0
Jim Sangwine <jim@sangwine.net>
2016-04-13 08:35:59 +0200

    Fixed launchd scripts

--- install/osx.sh
+++ install/osx.sh
@@ -1,19 +1,15 @@
 echo "symlinking launchd-scripts"
 ln -sfv "$DOTFILES_DIR/osx/launchd-scripts" $HOME
+for file in "$DOTFILES_DIR/osx/launchd-scripts"/*.sh; do
+  chmod +x "$DOTFILES_DIR/osx/launchd-scripts/${file##*/}"
+done
 
 for directory in LaunchAgents LaunchDaemons; do
-  if [ -d "$HOME/Library/$directory" ]; then
-    echo "found existing $directory"
-    for file in "$DOTFILES_DIR/osx/$directory"/*.plist; do
-      echo "symlinking $file"
-      ln -sfv "$DOTFILES_DIR/osx/$directory/${file##*/}" "$HOME/Library/$directory/"
-      launchctl load "$DOTFILES_DIR/osx/$directory/${file##*/}"
-    done
-  else
-    echo "symlinking $directory"
-    ln -sfv "$DOTFILES_DIR/osx/$directory" "$HOME/Library/"
-    for file in "$DOTFILES_DIR/osx/$directory"/*.plist; do
-      launchctl load "$DOTFILES_DIR/osx/$directory/${file##*/}"
-    done
+  if [ ! -d "$HOME/Library/$directory" ]; then
+    mkdir "$HOME/Library/$directory"
   fi
+  for file in "$DOTFILES_DIR/osx/$directory"/*.plist; do
+    ln -sfv "$DOTFILES_DIR/osx/$directory/${file##*/}" "$HOME/Library/$directory/"
+    launchctl load "$DOTFILES_DIR/osx/$directory/${file##*/}"
+  done
 done
