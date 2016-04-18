f483ea782b174b49fccab73cf68800d277541f88
Jim Sangwine <jim@sangwine.net>
2016-04-18 17:41:05 +0200

    Added support for ~/Library/Preferences & included preferences for VMWare Fusion

--- install/osx.sh
+++ install/osx.sh
@@ -1,9 +1,12 @@
-echo "symlinking launchd-scripts"
+echo "$(tput setaf 2)Starting OSX specific install$(tput sgr 0)"
+
+echo "Linking launchd-scripts"
 ln -sfv "$DOTFILES_DIR/osx/launchd-scripts" $HOME
 for file in "$DOTFILES_DIR/osx/launchd-scripts"/*.sh; do
   chmod +x "$DOTFILES_DIR/osx/launchd-scripts/${file##*/}"
 done
 
+echo "$(tput setaf 2)Linking LaunchAgents and LaunchDaemons$(tput sgr 0)"
 for directory in LaunchAgents LaunchDaemons; do
   if [ ! -d "$HOME/Library/$directory" ]; then
     mkdir "$HOME/Library/$directory"
@@ -13,3 +16,15 @@ for directory in LaunchAgents LaunchDaemons; do
     launchctl load "$DOTFILES_DIR/osx/$directory/${file##*/}"
   done
 done
+
+echo "$(tput setaf 2)Linking Preferences$(tput sgr 0)"
+for directory in "$DOTFILES_DIR/osx/Preferences"/*; do
+  if [ ! -d "$HOME/Library/Preferences/${directory##*/}" ]; then
+    mkdir "$HOME/Library/Preferences/${directory##*/}"
+  fi
+  for file in "$directory"/*; do
+    ln -sfv "$directory/${file##*/}" "$HOME/Library/Preferences/${directory##*/}/"
+  done
+done
+
+echo "$(tput setaf 2)Finished OSX specific install$(tput sgr 0)"
